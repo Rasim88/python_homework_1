d47388e24df5e534046d9161ed576e1651a98c7d
Rasim Fakhretdinov <111516877+Rasim88@users.noreply.github.com>
2022-11-12 08:02:26 +0500

    Add correct file's with homework for Python №6

--- 6_1.py
+++ 6_1.py
@@ -9,47 +9,6 @@
 # # 1+2*3 => 7;
 # # (1+2)*3 => 9;
 
-# from decimal import Decimal
-
-# def my_calc(expr):
-#     # while '(' in expr:
-#     #     pass
-
-#     num = ''
-#     oper = ''
-
-#     for ch in expr:
-#         if ch.isdigit() or ch == '.':
-#             num += ch
-#         elif ch == '-':
-#             num += ' ' + ch
-#         elif ch == ' ':
-#             continue
-#         else:
-#             oper += ch
-#             num += ' '
-#     num, oper = list(map(Decimal, num.split())), list(oper)
-
-
-#     def make_oper(sign:str):
-#         sign_index = oper.index()
-#         first_num = num[sign_index]
-#         second_num = num[sign_index + 1]
-#         if sign == '/':
-#             if second_num == 0:
-#                 print('You can not devide by zero')
-#                 exit()
-#             num[sign_index] = first_num / second_num
-#         elif sign == '*':
-#             num[sign_index] = first_num * second_num
-#         else:
-#             num[sign_index] = first_num + second_num
-#         del num[sign_index+1]
-#         del oper[sign_index]
-
-#     for i in '/*+':
-#         make_oper(i)
-
 orig_expr = input('Enter the arithmetic expression without a spaces: ')
-final_expr = eval(orig_expr)
+final_expr = eval(orig_expr) #возвращает результат выполнения строки
 print(final_expr)
